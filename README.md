39757a7be7a8502793b54246268f37fa56d6ba48
Alex Tompkins <alex@atomic.nz>
2025-09-18 23:10:38 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# `vite-plugin-shadow-styles`
+
+Placeholder
---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import { globalIgnores } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    plugins: {
+      'simple-import-sort': simpleImportSortPlugin,
+    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.browser,
+    },
+    settings: {
+      'import/extensions': ['.js', '.jsx', 'ts', 'tsx'],
+      'import/resolver': { typescript: true, node: true },
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+
+      // Import sorting
+      'import/order': 'off', // must be off for simple-import-sort to work
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+      'sort-imports': 'off', // must be off for simple-import-sort to work
+    },
+  },
+]);
